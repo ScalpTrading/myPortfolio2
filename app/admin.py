45cb5a46a0b5af411c5a1719a42615c251ee4cea
Chris Hui <christopher.slhui@gmail.com>
2021-05-30 13:49:46 +0100

    Added watchlist model

--- app/admin.py
+++ app/admin.py
@@ -4,5 +4,11 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
+from .models import Watchlist
 
 # Register your models here.
+
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'symbol')
+
+admin.site.register(Watchlist, WatchlistAdmin)
--- app/models.py
+++ app/models.py
@@ -8,3 +8,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    symbol = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.user} is watching {self.symbol}"
